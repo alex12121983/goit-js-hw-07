f3fea8d63e1b7446b86fd3efa9da45e2697734a3
Alex Taras <alex12121983@gmail.com>
2023-02-13 20:13:15 +0100

    Task#2 is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+ 
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+gallery.innerHTML = galleryItems.reduce(
+    (html, current) => 
+    html + `
+    <a class="gallery__item" href="${current.original}">
+        <img class="gallery__image" src="${current.preview}" alt="${current.description}" />
+    </a>
+    `, ""
+    );
+
+    let lightbox = new SimpleLightbox('.gallery a', { 
+      captionsData: "alt",
+      captionDelay: 250,
+    });
